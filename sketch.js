9e96a221ba98dcb56280a91f922b4e1800ed5cd3
Saad Elkheety <SaadElkheety@gmail.com>
2023-03-26 16:02:42 +0200

    make it more elegant

--- sketch.js
+++ sketch.js
@@ -40,23 +40,21 @@ function setup()
 function draw()
 {   
 	///////////DRAWING CODE//////////
-
 	background(100,155,255); //fill the sky blue
 
 	noStroke();
 	fill(0,155,0);
 	rect(0, floorPos_y, width, height - floorPos_y); //draw some green ground
-    
+
     checkPlayerDie();
     // Implement scrolling
     cameraPosX = cameraPosX + gameChar_x - old_gameChar_x;
     old_gameChar_x = gameChar_x;
-    
     push();
     translate(-cameraPosX, 0);
 
 	//draw the canyon
-    for(var i = 0; i < canyons.length; i++)
+    for (var i = 0; i < canyons.length; i++)
     {
         drawCanyon(canyons[i]);
         //detect when the character is over the canyon
@@ -71,13 +69,14 @@ function draw()
     drawTrees();
     // poleflag
     renderFlagpole()
-    if(flagpole.isReached == false)
+    
+    if (flagpole.isReached == false)
     {
        checkFlagpole();
     }
     
     //a collectable token - eg. a jewel, fruit, coins
-    for(var i = 0; i < collectables.length; i++)
+    for (var i = 0; i < collectables.length; i++)
     {
         if (!collectables[i].isFound)
         {
@@ -94,108 +93,84 @@ function draw()
     }
     
 	//the game character
-	if(isLeft && isFalling)
+	if (isLeft && isFalling)
 	{
 		// add your jumping-left code
-        
         // draw the face
         fill(213, 216, 220);
         ellipse(gameChar_x , gameChar_y - 60, 20, 30);
-
         // draw the body
         fill(0, 0, 256);
         rect(gameChar_x - 8, gameChar_y - 45, 16, 30);
-
         // draw hands
         fill(0);
         rect(gameChar_x - 23, gameChar_y - 45, 26, 6);
-
         // draw the feets
         fill(0);
         rect(gameChar_x - 5, gameChar_y - 15, 10, 8);
-
 	}
-	else if(isRight && isFalling)
+	else if (isRight && isFalling)
 	{
 		// add your jumping-right code
-        
         // draw the face
         fill(213, 216, 220);
         ellipse(gameChar_x , gameChar_y - 60, 20, 30);
-
         // draw the body
         fill(0, 0, 256);
         rect(gameChar_x - 8, gameChar_y - 45, 16, 30);
-
         // draw hands
         fill(0);
         rect(gameChar_x - 3, gameChar_y - 45, 26, 6);
-
         // draw the feets
         fill(0);
         rect(gameChar_x - 5, gameChar_y - 15, 10, 8);
-        
-
 	}
-	else if(isLeft)
+	else if (isLeft)
 	{
 		// add walking left code
         
         // draw the face
         fill(213, 216, 220);
         ellipse(gameChar_x , gameChar_y - 60, 20, 30);
-
         // draw the body
         fill(0, 0, 256);
         rect(gameChar_x - 8, gameChar_y - 45, 16, 40);
-
         // draw hands
         fill(0);
         rect(gameChar_x - 23, gameChar_y - 45, 26, 6);
-
         // draw the feets
         fill(0);
         rect(gameChar_x - 5, gameChar_y - 5, 10, 8); 
-
 	}
-	else if(isRight)
+	else if (isRight)
 	{
 		// add walking right code
-        
         // draw the face
         fill(213, 216, 220);
         ellipse(gameChar_x , gameChar_y - 60, 20, 30);
-
         // draw the body
         fill(0, 0, 256);
         rect(gameChar_x - 8, gameChar_y - 45, 16, 40);
-
         // draw hands
         fill(0);
         rect(gameChar_x - 3, gameChar_y - 45, 26, 6);
-
         // draw the feets
         fill(0);
         rect(gameChar_x - 5, gameChar_y - 5, 10, 8);
-
 	}
-	else if(isFalling || isPlummeting)
+	else if (isFalling || isPlummeting)
 	{
 		// add jumping facing forwards code
-        
         // draw the face
         fill(213, 216, 220);
         ellipse(gameChar_x , gameChar_y - 60, 26, 30);
-
         // draw the body
         fill(0, 0, 256);
         rect(gameChar_x - 15, gameChar_y - 45, 30, 30);
-
         // draw hands
         fill(0);
         rect(gameChar_x - 21, gameChar_y - 65, 6, 28);
         rect(gameChar_x + 15, gameChar_y - 65, 6, 28);
-
         // draw the feets
         fill(0);
         rect(gameChar_x - 15, gameChar_y - 15, 10, 8); 
@@ -206,20 +181,16 @@ function draw()
 	else
 	{
 		// add your standing front facing code
-        
         // draw the face
         fill(213, 216, 220);
         ellipse(gameChar_x , gameChar_y - 60, 26, 30);
-
         // draw the body
         fill(0, 0, 256);
         rect(gameChar_x - 15, gameChar_y - 45, 30, 40);
-
         // draw hands
         fill(0);
         rect(gameChar_x - 21, gameChar_y - 45, 6, 28);
         rect(gameChar_x + 15, gameChar_y - 45, 6, 28);
-
         // draw the feets
         fill(0);
         rect(gameChar_x - 15, gameChar_y - 5, 10, 8); 
@@ -228,7 +199,7 @@ function draw()
 	}
     pop();
     
-    for(var i=0; i<lives; i++)
+    for (var i=0; i<lives; i++)
     {
         // draw life tokens
         // add your standing front facing code
@@ -238,16 +209,13 @@ function draw()
         var divider = 5
         fill(213, 216, 220);
         ellipse(lifeChar_x , lifeChar_y - 60/divider, 26/divider, 30/divider);
-
         // draw the body
         fill(0, 0, 256);
         rect(lifeChar_x - 15/divider, lifeChar_y - 45/divider, 30/divider, 40/divider);
-
         // draw hands
         fill(0);
         rect(lifeChar_x - 21/divider, lifeChar_y - 45/divider, 6/divider, 28/divider);
         rect(lifeChar_x + 15/divider, lifeChar_y - 45/divider, 6/divider, 28/divider);
-
         // draw the feets
         fill(0);
         rect(lifeChar_x - 15/divider, lifeChar_y - 5/divider, 10/divider, 8/divider); 
@@ -257,14 +225,14 @@ function draw()
     fill(255);
     text('score: ' + game_score, 20, 15);
     
-    if(lives < 1)
+    if (lives < 1)
     {
         fill(255);
         text("Game over. Press space to continue.", width/2, height/2);
         return;
     }
     
-    if(flagpole.isReached)
+    if (flagpole.isReached)
     {
         fill(255);
         text("Level complete. Press space to continue.", width/2, height/2);
@@ -273,17 +241,17 @@ function draw()
     
 	///////////INTERACTION CODE//////////
 	//Put conditional statements to move the game character below here
-    if(isLeft)
+    if (isLeft)
     {
         gameChar_x -= 3;
     }
     
-    if(isRight)
+    if (isRight)
     {
         gameChar_x += 3;
     }
     
-    if(gameChar_y < floorPos_y)
+    if (gameChar_y < floorPos_y)
     {
         isFalling  = true;
         gameChar_y = min(gameChar_y + 5, floorPos_y);
@@ -293,7 +261,7 @@ function draw()
         isFalling  = false;
     }
     
-    if(isJumping)
+    if (isJumping)
     {
        gameChar_y -= 180;
        isJumping = false;
@@ -308,7 +276,6 @@ function keyPressed()
 {
 	// if statements to control the animation of the character when
 	// keys are pressed.
-
 	//open up the console to see how these work
     if(keyCode == 39 && !isPlummeting)
     {
@@ -330,13 +297,12 @@ function keyReleased()
 {
 	// if statements to control the animation of the character when
 	// keys are released.
-    
-    if(keyCode == 39)
+    if (keyCode == 39)
     {
         isRight = false;
     }
     
-    if(keyCode == 37)
+    if (keyCode == 37)
     {
         isLeft = false;
     }
@@ -351,7 +317,7 @@ function keyReleased()
 
 function drawClouds()
 {
-    for(var i = 0; i < clouds.length; i++)
+    for (var i = 0; i < clouds.length; i++)
     {
         noStroke();
         fill(255);
@@ -365,7 +331,7 @@ function drawClouds()
 
 function drawMountains()
 {
-    for(var i = 0; i < mountains.length; i++)
+    for (var i = 0; i < mountains.length; i++)
     {
         fill(180);
         triangle(mountains[i].x_pos, floorPos_y, mountains[i].x_pos + mountains[i].width, floorPos_y, mountains[i].x_pos + mountains[i].width/2, floorPos_y-mountains[i].height);
@@ -376,7 +342,7 @@ function drawMountains()
 
 function drawTrees()
 {
-    for(var i = 0; i < trees_x.length; i++)
+    for (var i = 0; i < trees_x.length; i++)
     {
         fill(148);
         rect(trees_x[i], treePos_y - 100, 50, 100);
@@ -407,7 +373,7 @@ function drawCanyon(t_canyon)
 
 function checkCollectable(t_collectable)
 {
-    if(dist(gameChar_x, gameChar_y, t_collectable.x_pos, t_collectable.y_pos) < 25 + t_collectable.size/2)
+    if (dist(gameChar_x, gameChar_y, t_collectable.x_pos, t_collectable.y_pos) < 25 + t_collectable.size/2)
     {
         t_collectable.isFound = true;
         game_score++
@@ -416,7 +382,7 @@ function checkCollectable(t_collectable)
 
 function checkCanyon(t_canyon)
 {
-    if(gameChar_x > t_canyon.x_pos + 14 && gameChar_x < t_canyon.x_pos + t_canyon.width - 14 && gameChar_y >= floorPos_y)
+    if (gameChar_x > t_canyon.x_pos + 14 && gameChar_x < t_canyon.x_pos + t_canyon.width - 14 && gameChar_y >= floorPos_y)
     {
         isPlummeting = true;
     }
@@ -431,7 +397,7 @@ function renderFlagpole()
     line(flagpole.x_pos, floorPos_y , flagpole.x_pos, floorPos_y-200);
     noStroke();
     fill(255, 0, 0);
-    if(flagpole.isReached)
+    if (flagpole.isReached)
     {
         rect(flagpole.x_pos, floorPos_y-200, 50, 40);
     }
@@ -440,7 +406,7 @@ function renderFlagpole()
 
 function checkFlagpole()
 {
-    if(abs(gameChar_x-flagpole.x_pos) < 20)
+    if (abs(gameChar_x-flagpole.x_pos) < 20)
     {
         flagpole.isReached = true;
     }
@@ -448,10 +414,10 @@ function checkFlagpole()
 
 function checkPlayerDie()
 {
-    if(gameChar_y > height)
+    if (gameChar_y > height)
     {
         lives--;
-        if(lives > 0)
+        if (lives > 0)
         {
             startGame();
         }
@@ -463,21 +429,17 @@ function startGame()
     gameChar_x = width/2;
 	gameChar_y = floorPos_y;
     game_score = 0;
-    
     //These variables will be used to animate your game character.
     isLeft = false;
     isRight = false;
     isFalling = false;
     isPlummeting = false;
-    
     trees_x = [100, 300, 600, 900, 1200, 1600, 6000, 9000, 12000];
     treePos_y = floorPos_y;
-    
     flagpole = {
         x_pos: width*2,
         isReached: false
     };
-    
     collectables = [
         {x_pos: width/6,      y_pos: floorPos_y, size: 30, isFound: false},
         {x_pos: width/2.2,    y_pos: floorPos_y, size: 30, isFound: false},
@@ -490,7 +452,6 @@ function startGame()
         {x_pos: width * 2.4,  y_pos: floorPos_y, size: 30, isFound: false},
         {x_pos: width * 2.7,  y_pos: floorPos_y, size: 30, isFound: false}
     ];
-    
     canyons = [
         {x_pos: width*0.25, width: 60},
         {x_pos: width*0.75, width: 60},
@@ -501,7 +462,6 @@ function startGame()
         {x_pos: width*3.75, width: 60},
         {x_pos: width*3.75, width: 60},
     ];
-    
     clouds = [
         {x_pos: 150,  y_pos: 50 ,width: 150},
         {x_pos: 1400, y_pos: 40 ,width: 130},
@@ -512,7 +472,6 @@ function startGame()
         {x_pos: 3700, y_pos: 55 ,width: 120},
         {x_pos: 4150, y_pos: 48 ,width: 70 }
     ];
-    
     mountains = [
         {x_pos: width / 5,      width: 250, height: 300},
         {x_pos: width * 3,    width: 150, height: 300},
@@ -523,7 +482,6 @@ function startGame()
         {x_pos: width * 4,    width: 450, height: 300},
         {x_pos: width * 5,    width: 500, height: 300},
     ];
-    
     cameraPosX = 0;
     old_gameChar_x = gameChar_x;
 }
